fa88fbcfcec4109fc1d109ca980c642311adf45e
Luis Granados <luisoswaldogra@gmail.com>
2024-05-09 22:59:29 -0600

    Corrección de Containers

--- arcane/src/Personajes.js
+++ arcane/src/Personajes.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+
+
 class Personajes extends Component {
   render() {
     const myDiv = document.querySelector('div');
@@ -10,8 +12,10 @@ class Personajes extends Component {
 
         <body>
         <section class = "diseño">
-          <div class="container">
 
+        
+
+        <div class="container">
           <div class = "card">
             <img src = "https://phantom-marca.unidadeditorial.es/a640bee7a22bc819a1178e56cfd18550/resize/990/f/webp/assets/multimedia/imagenes/2021/09/27/16327333026585.jpg" alt="Personaje1" class="tarjeta"></img>
             <div class = "intro">
@@ -19,7 +23,8 @@ class Personajes extends Component {
               <p>Una luchadora con el pelo rosa que tuvo una infancia difícil pero sigue teniendo muy buen corazón. Eso sí, sus métodos son un tanto atrevidos. En Arcane, repasaremos el pasado de Vi y veremos cómo pasa de ser una mezquina criminal de Zaun a protectora de la paz en Piltover.</p>
             </div>
           </div>
-
+          
+          
           <div class = "card">
             <img src = "https://phantom-marca.unidadeditorial.es/cf0fef865d1c3395c8a22be42dfa56d0/resize/990/f/webp/assets/multimedia/imagenes/2021/09/27/16327333036439.jpg" alt="Personaje1" class="tarjeta"></img>
             <div class = "intro">
@@ -27,7 +32,9 @@ class Personajes extends Component {
               <p>La hermana de Vi es un tanto complicada. Con una mente a otro nivel y una insensatez implacable, la inteligencia y el pasado traumático de Jinx la convierten en una villana estupenda.</p>
             </div>
           </div>
+          
 
+          
           <div class = "card">
             <img src = "https://phantom-marca.unidadeditorial.es/12395e3955a95a292ddd5b666b3eea80/resize/990/f/webp/assets/multimedia/imagenes/2021/09/27/16327333054826.jpg" alt="Personaje1" class="tarjeta"></img>
             <div class = "intro">
@@ -35,7 +42,9 @@ class Personajes extends Component {
               <p>Conocido como el Defensor del mañana de Piltover, Jayce es un inventor que se ha convertido en un líder político. Fue una pieza fundamental en la invención de la tecnología hextech, una forma científica de dominar la energía arcana.</p>
             </div>
           </div>
+          </div>
           
+          <div class="container">
           <div class = "card">
             <img src = "https://phantom-marca.unidadeditorial.es/1e7061211df39aa22b3a5ff1fe00d746/resize/990/f/webp/assets/multimedia/imagenes/2021/09/27/16327333061804.jpg" alt="Personaje1" class="tarjeta"></img>
             <div class = "intro">
@@ -43,7 +52,9 @@ class Personajes extends Component {
               <p>Caitlyn es tenaz y decidida, además de la oveja negra de su familia, e intenta constantemente huir de los tentáculos de su influencia. Adora a su familia, pero reniega del privilegio de nacer en un hogar adinerado, ya que considera la riqueza una prisión.</p>
             </div>
           </div>
+          
 
+          
           <div class = "card">
             <img src = "https://phantom-marca.unidadeditorial.es/b07b375d5d8e6a795b524dd256239958/resize/990/f/webp/assets/multimedia/imagenes/2021/09/27/16327333067078.jpg" alt="Personaje1" class="tarjeta"></img>
             <div class = "intro">
@@ -59,7 +70,9 @@ class Personajes extends Component {
               <p>Nacido en Zaun, la gran mente de Viktor lo ayudó a llegar hasta Piltover. Se convirtió en el compañero de Jayce durante el desarrollo de la hextech, pero la enfermedad de Viktor le lleva a explorar los límites de lo que puede hacer esta nueva tecnología.</p>
             </div>
           </div>
-
+          </div>
+          
+          <div class="container">
           <div class = "card">
             <img src = "https://phantom-marca.unidadeditorial.es/1bd111fc05d88beb98b6e2562147b48f/resize/990/f/webp/assets/multimedia/imagenes/2021/09/27/16327333042631.jpg" alt="Personaje1" class="tarjeta"></img>
             <div class = "intro">
@@ -67,7 +80,9 @@ class Personajes extends Component {
               <p>Aunque es poderoso en la ciudad subterránea, Vander se centra en mantener a salvo a los ciudadanos de Zaun de los agentes de Piltover, que a menudo actúan con mano dura.</p>
             </div>
           </div>
+          
 
+          
           <div class = "card">
             <img src = "https://phantom-marca.unidadeditorial.es/cff7297906d3e35f322928cbeaf4f7bc/resize/990/f/webp/assets/multimedia/imagenes/2021/09/27/16327333049137.jpg" alt="Personaje1" class="tarjeta"></img>
             <div class = "intro">
@@ -76,6 +91,8 @@ class Personajes extends Component {
             </div>
           </div>
           </div>
+          
+
           </section>
           </body>
         
